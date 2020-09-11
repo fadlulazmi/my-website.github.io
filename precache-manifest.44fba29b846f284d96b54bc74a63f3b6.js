31c4ae6a81a93bbd1bbdc93febb31860f48662d3
telkomdev-fadlulazmi <fadlulazmi17@gmail.com>
2020-09-12 02:38:43 +0700

    new home page

---
+++ precache-manifest.44fba29b846f284d96b54bc74a63f3b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faa40cdac75980643e9c82d4c73fb5ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31417df1d9eaa99a5dad",
+    "url": "/static/css/main.6b44a8c9.chunk.css"
+  },
+  {
+    "revision": "f5cb13d5fc33e94126e3",
+    "url": "/static/js/2.f85388ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f85388ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31417df1d9eaa99a5dad",
+    "url": "/static/js/main.6308b354.chunk.js"
+  },
+  {
+    "revision": "420d11f43645d04fc865",
+    "url": "/static/js/runtime-main.9b126251.js"
+  }
+]);

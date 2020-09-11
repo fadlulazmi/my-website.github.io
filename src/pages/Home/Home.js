3d00ecf6a53ceed69a934f85914061273fe2e053
telkomdev-fadlulazmi <fadlulazmi17@gmail.com>
2020-09-12 02:28:19 +0700

    build home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 export default function Home() {
     return (
-        <div>
-            WELCOME To MY HOME :)
-        </div>
+        <section className="home">
+            <div></div>
+            <div>
+                <p className="animate">Hi.</p>
+                <p className="animate">I am</p>
+                <p className="animate">Fadlul Azmi</p>
+            </div>
+        </section>
     )
 }
